31028906fe2da7c43077ceed14b6f6282a08eb50
firas1996 <firas.hm.1996@gmail.com>
2026-04-17 15:04:55 +0100

    a

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,48 @@
+import { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handelChange = ({ target }) => {
+    const { name, value } = target;
+    setUserData({ ...userData, [name]: value });
+  };
+  const loginHandler = () => {
+    console.log(userData);
+    setUserData({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <div className="container">
       <div>
         <div className="divInp">
           <label>Email</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={handelChange}
+            name="email"
+            value={userData.email}
+          />
         </div>
         <div className="divInp">
           <label>Password</label>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={handelChange}
+            name="password"
+            value={userData.password}
+          />
         </div>
       </div>
       <div>
-        <button>Login</button>
+        <button onClick={loginHandler}>Login</button>
       </div>
     </div>
   );
