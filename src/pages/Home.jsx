0fbe276ee94b3ed239c58718a5a9186bb721d322
firas1996 <firas.hm.1996@gmail.com>
2026-04-24 15:54:47 +0100

    aa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,17 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
   const logoutHandler = () => {
+    localStorage.removeItem("token");
     navigate("/login");
   };
 
   return (
     <div>
-      <h1>Welcome Back .......</h1>
       <button onClick={logoutHandler}>Logout</button>
+      <ul>
+        <li>user1</li>
+        <li>user2</li>
+      </ul>
     </div>
   );
 };
